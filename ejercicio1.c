69ed7b0f81f31de58c8447eb83675491f49116b5
miguelalejandro91 <miguel.rodriguez.calderon@udla.edu.ec>
2023-06-13 12:37:47 -0500

    1

---
+++ ejercicio1.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    
+
+    // declaracion de punteros y variables
+    int *ptrI, varI;
+    char *ptrC, varC;
+    float *ptrF, varF;
+
+    //RELACION ENTRE PUNTERO Y VARIABLE
+ 
+    ptrI = &varI;
+    ptrC = &varC;
+    ptrF = &varF;
+
+*ptrI = 89;
+*ptrC = 56;
+*ptrF = 45;
+
+printf("%d %p\n",varI, ptrI);
+printf("%d %p\n",varC, ptrC);
+printf("%c %p\n",varF, ptrF);
+
+
+return 0;
+}
