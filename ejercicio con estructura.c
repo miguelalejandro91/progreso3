5e780ef13e6531b76f79e99a9c3c865e0f501326
miguelalejandro91 <miguel.rodriguez.calderon@udla.edu.ec>
2023-06-20 13:06:24 -0500

    3

--- ejercicio con estructura.c
+++ ejercicio con estructura.c
@@ -13,18 +13,24 @@ int main() {
     struct Alumno alumnos[3];
 
     for (int i = 0; i < 3; i++) {
+
+        
         printf("Ingrese los datos del alumno %d:\n", i+1);
         
+        // lee el nombre del alumno
         printf("Nombre: ");
         scanf("%s", alumnos[i].nombre);
         fflush(stdin);
         
+        // lee la direccion del alumno
         printf("Direccion: ");
         gets(alumnos[i].direccion);
         
+        // lee la carrera del alumno
         printf("Carrera: ");
         gets(alumnos[i].carrera);
         
+        // lee el promedio del alumno
         printf("Promedio: ");
         scanf("%f", &alumnos[i].promedio);
         fflush(stdin);
@@ -34,8 +40,8 @@ int main() {
 
     printf("Datos de los alumnos:\n");
     for (int i = 0; i < 3; i++) {
-        printf("Alumno %d:\n", i+1);
-        printf("Nombre: %s\n", alumnos[i].nombre);
+        printf("Alumno %d:\n", i+1); 
+        printf("Nombre: %s\n", alumnos[i].nombre); 
         printf("Direccion: %s\n", alumnos[i].direccion);
         printf("Carrera: %s\n", alumnos[i].carrera);
         printf("Promedio: %.2f\n\n", alumnos[i].promedio);
