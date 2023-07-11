48173f89b2901d550c40cd899985d7d594effec9
miguelalejandro91 <miguel.rodriguez.calderon@udla.edu.ec>
2023-07-11 11:43:35 -0500

    encabezado

--- examenp3.c
+++ examenp3.c
@@ -1,1 +1,10 @@
 // EXAMEN PROGRESO 3
+
+/* Construya un programa que, al leer el archivo de texto subido 
+al aula virtual y que contiene errores ortográficos en la palabra 
+"Ecuador", cada vez que localice esta palabra, reemplace la primera 
+letra con mayúscula y escriba el nuevo texto corregido en otro archivo 
+diferente. Además, muestre en pantalla cuantas palabras se corrigió en 
+total. Debe utilizar el concepto de "Función". */
+
+
